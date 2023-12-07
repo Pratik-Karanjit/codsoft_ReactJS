f37e0be11212a5be610b797e3a966d602e486cca
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-12-07 10:08:53 +0545

    tasks

--- task-tracker-app/src/App.js
+++ task-tracker-app/src/App.js
@@ -1,45 +1,74 @@
-import React from 'react'
-import './styles.css'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const App = () => {
+  // State Hooks
+  const [showTaskForm, setShowTaskForm] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-  //UseState Hooks 
-  let [task, setTask] = useState()
-  let [incompleteTask, setIncompleteTask] = useState()
-  let [completeTask, setCompleteTask] = useState()
+  // Function to handle task input change
+  const handleTaskChange = (event) => {
+    setNewTask(event.target.value);
+  };
 
+  // Function to handle form submission
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      // Add the new task to the ongoing tasks list
+      setTasks([...tasks, newTask]);
+      // Clear the input field
+      setNewTask('');
+      // Hide the task form
+      setShowTaskForm(false);
+    }
+  };
 
   return (
-    //Main div
     <div className='container'>
-        <div className='header'>
-          {/* heading in h2 */}
+      <div className='header'>
         <h2>Task Tracker App</h2>
-        <button className='add-task'>Add Tasks</button>
+        <button className='add-task' onClick={() => setShowTaskForm(true)}>
+          Add Tasks
+        </button>
       </div>
+
       <div className='row row-cards'>
-      <div class="col-card">
-        <b>Ongoing Tasks</b>
-        <div class="col-bg">
-        
+        <div className='col-card'>
+          <b>Ongoing Tasks</b>
+          <div className='col-bg'>
+            {/* Display ongoing tasks */}
+            {tasks.map((task, index) => (
+              <div key={index}>{task}</div>
+            ))}
+          </div>
         </div>
-     </div>
-      <div class="col-card">
-      <b>Incomplete Tasks</b>
-        <div class="col-bg">
-        
+
+        {/* Add Task Form */}
+        {showTaskForm && (
+          <div className='add-task-form'>
+            <input
+              type='text'
+              placeholder='Enter task...'
+              value={newTask}
+              onChange={handleTaskChange}
+            />
+            <button onClick={handleAddTask}>Save Task</button>
+          </div>
+        )}
+
+        <div className='col-card'>
+          <b>Incomplete Tasks</b>
+          <div className='col-bg'>{/* Display incomplete tasks */}</div>
         </div>
-     </div>
-      <div class="col-card">
-      <b>Completed Tasks</b>
-        <div class="col-bg">
-        {/* &nbsp;&nbsp; */}
+
+        <div className='col-card'>
+          <b>Completed Tasks</b>
+          <div className='col-bg'>{/* Display completed tasks */}</div>
         </div>
-     </div>
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
